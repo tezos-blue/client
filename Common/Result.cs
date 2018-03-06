f728713b1915e5e71334b980b0cb3946d8c1c89b
Andreas Hassmann <hello@tezos.blue>
2018-03-06 21:30:27 +0100

    generic result type

---
+++ Common/Result.cs
@@ -0,0 +1,33 @@
+﻿namespace SLD.Tezos
+{
+	/// <summary>
+	/// A pseudo bool with capability to transport an error id, when failed.
+	/// Alternative return value to indicate execution success or failure
+	/// </summary>
+	public struct Result
+	{
+		public const string GenericError = nameof(GenericError);
+
+		public string ErrorID { get; private set; }
+
+		public bool IsOK => !IsError;
+
+		public bool IsError => ErrorID != null;
+
+		#region Creation
+
+		public static Result OK => new Result();
+
+		public static Result Error(string errorID = GenericError) => new Result { ErrorID = errorID };
+
+		#endregion Creation
+
+		#region Boolean conversions
+
+		public static implicit operator bool(Result result) => result.IsOK;
+
+		public static implicit operator Result(bool result) => result ? OK : Error();
+
+		#endregion Boolean conversions
+	}
+}
